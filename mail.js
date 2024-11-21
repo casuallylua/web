8104f21064ea4a7a4bbf1aa8ac567c04e962f0cf
casuallylua <casuallylua@gmail.com>
2024-11-20 22:35:32 -0500

    Create mail.js

---
+++ mail.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC2j23SpgCFBSBfOiXSc8TJAgspNJJJ-0c",
+  authDomain: "my-portfolio-e26c0.firebaseapp.com",
+  projectId: "my-portfolio-e26c0",
+  storageBucket: "my-portfolio-e26c0.firebasestorage.app",
+  messagingSenderId: "322879771293",
+  appId: "1:322879771293:web:68f1280d8783a15e927a74",
+  measurementId: "G-6WW8FBX4LP"
+};
