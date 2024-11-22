501546e83eafb4259a3a07401bf3d36aae3eec1b
casuallylua <casuallylua@gmail.com>
2024-11-21 22:46:41 -0500

    Update mail.js

--- mail.js
+++ mail.js
@@ -7,3 +7,15 @@ const firebaseConfig = {
   appId: "1:322879771293:web:68f1280d8783a15e927a74",
   measurementId: "G-6WW8FBX4LP"
 };
+// init firebase
+firebase.initializeApp(firebaseConfig);
+
+//reference
+var contactFormDB = firebase.database().ref("contactForm");
+
+document.getElementById('contactForm').addEventListener('submit', submitForm)
+
+function submitForm(e) {
+  e.preventDefault();
+}
+
